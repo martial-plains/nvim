6a9800961657161a94416ad6732e42b9b79fa6c9
Allister Isaiah Harvey <a.i.harvey@icloud.com>
2022-06-25 06:25:15 -0400

    Update telescope.lua

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -109,3 +109,6 @@ telescope.setup({
 })
 -- telescope.load_extension "ui-select"
 telescope.load_extension("file_browser")
+telescope.load_extension("ui-select")
+telescope.load_extension("media_files")
+telescope.load_extension('vim_bookmarks')
